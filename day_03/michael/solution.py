0cfdeeb6abc07f6b306985925357e86f9a8e81c0
Michael Willig <michael.willig@gs1.nl>
2022-12-03 07:12:39 +0100

    day 3

--- day_03/michael/solution.py
+++ day_03/michael/solution.py
@@ -1,13 +1,5 @@
 import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
-from copy import deepcopy
-from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
+import string
 from functools import reduce
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
@@ -22,8 +14,28 @@ def read_input_text():
         return fh.read().strip()
 
 
+symb2score = {let:score +1 for let,score in zip(string.ascii_letters,range(52))}
+
 def part_a():
-    pass
+
+    def get_line_score(line):
+        halfway = len(line) // 2
+        symbol = (set(line[:halfway]) & set(line[halfway:])).pop()
+        return symb2score[symbol]
+
+    print(sum(get_line_score(line) for line in read_input_lines()))
 
 def part_b():
-    pass
+
+    def get_batch_score(batch):
+        sets = [set(line) for line in batch]
+        symbol =reduce(lambda x,y: x & y,sets).pop()
+        return symb2score[symbol]
+
+    lines = read_input_lines()
+    batched_lines = [lines[i:i+3] for i in range(0, len(lines), 3)]
+    print(sum(get_batch_score(batch) for batch in batched_lines))
+
+
+part_a()
+part_b()
