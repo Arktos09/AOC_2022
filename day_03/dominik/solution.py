ee099805fa5fa2236caf9794449c7f70040e4ec5
Dominik Loroch <36048261+Arktos09@users.noreply.github.com>
2022-12-03 11:15:17 +0100

    Day 3: 2 Gold Stars

--- day_03/dominik/solution.py
+++ day_03/dominik/solution.py
@@ -12,7 +12,33 @@ def read_input_text():
 
 
 def puzzle1():
-    pass
+    backpacks= load_input_list()
+    prio_sum=0
+    for bag in backpacks:
+        comp1, comp2=set(bag[:int(len(bag)/2)]), set(bag[int(len(bag)/2):])
 
+        duplette=list(comp1.intersection(comp2))[0]
+        if duplette.isupper():
+            prio_sum+=ord(duplette)-38
+        else:
+            prio_sum+=ord(duplette)-96
+    return prio_sum
+# Unicode upper Case 65-90
+# Unicode lower case 97-122
 def puzzle2():
-    pass
+    backpacks= load_input_list()
+    groups= [backpacks[x:x+3] for x in range(0, len(backpacks), 3)]
+    prio_sum=0
+
+    for g in groups:
+        prio=list(set(g[0]).intersection(set(g[1])).intersection(set(g[2])))[0]
+        if prio.isupper():
+            prio_sum+=ord(prio)-38
+        else:
+            prio_sum+=ord(prio)-96
+    return prio_sum
+if __name__ == '__main__':
+    a=puzzle1()
+    print(f"Answer to puzzle a is: {a}")
+    b=puzzle2()
+    print(f"Answer to puzzle b is: {b}")
