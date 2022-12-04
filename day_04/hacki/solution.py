c2f9c223a8b4a12ef1cb27b50ec643ca820b4156
Shergotty <sebastian.hackler@gmx.de>
2022-12-04 11:27:21 +0100

    Update solution.py

--- day_04/hacki/solution.py
+++ day_04/hacki/solution.py
@@ -7,8 +7,8 @@ def get_fileinput(filename: str) -> list:
     return [x.strip() for x in content]
 
 
-filename = 'debug'
-# filename = 'day_4_1.txt'
+# filename = 'debug'
+filename = 'day_4_1.txt'
 
 puzzle_list = get_fileinput(filename)
 
@@ -19,59 +19,35 @@ def get_ranges(linst_input: list) -> list:
     return [[list(map(int, i.split('-'))) for i in p] for p in pairs]
 
 
-def joiner(list_input: list) -> list:
-
-    joined = ''.join([str(item)
-                     for item in range(list_input[0], list_input[-1]+1, 1)])
-    return joined
-
-
-def extract_ranges(list_input: list) -> list:
-
-    pair_range = [[joiner(pair) for pair in pairs] for pairs in list_input]
-    return pair_range
-
-
-def simple_comparer(list_input: list) -> int:
-
-    if list_input[0] in list_input[1] or list_input[1] in list_input[0]:
-        counter = 1
-    else:
-        counter = 0
-    print(list_input[0], list_input[1], counter)
-    return counter
-
-
-def counter(list_input: list) -> int:
-
-    return sum([simple_comparer(pair) for pair in list_input])
-
-
 def ranger(list_input: list) -> range:
-    return range(list_input[0], list_input[-1], 1)
+    return range(list_input[0], list_input[-1]+1, 1)
 
 
-def set_comparer(list_input: list) -> int:
+def set_comparer_a(list_input: list) -> int:
 
-    if set(list_input[0]).issubset(list_input[1]):
-        counter = 1
-    elif set(list_input[1]).issubset(list_input[0]):
-        counter = 1
-    else:
-        counter = 0
+    counter = 0
+    if set(list_input[0]).issubset(list_input[1]) or set(list_input[1]).issubset(list_input[0]):
+        counter += 1
 
     return counter
 
+def set_comparer_b(list_input: list) -> int:
 
+    counter = 0
+    if set(list_input[0]).intersection(list_input[1]) or set(list_input[1]).intersection(list_input[0]):
+        counter += 1
 
+    return counter
 
 
 def get_range_pairs(list_input: list) -> list:
-    return [set_comparer([ranger(p) for p in pair]) for pair in [pairs for pairs in list_input]]
+    
+    prepared_pairs = [pairs for pairs in list_input]
 
+    a = [set_comparer_a([ranger(p) for p in pair]) for pair in prepared_pairs]
+    b = [set_comparer_b([ranger(p) for p in pair]) for pair in prepared_pairs]
+    return a, b
 
 ranges = get_ranges(puzzle_list)
-str_ranges = extract_ranges(ranges)
 
-print(counter(str_ranges))
-print(sum(get_range_pairs(ranges)))
+print([sum(i) for i in get_range_pairs(ranges)])
