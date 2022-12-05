17cdf6f5635b87214fe270193a95a9cffb131d4d
Michael Willig <michael.willig@gs1.nl>
2022-12-05 08:54:42 +0100

    day 5

--- day_05/michael/solution.py
+++ day_05/michael/solution.py
@@ -1,14 +1,7 @@
 import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
-from copy import deepcopy
-from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
-from functools import reduce
+
+from collections  import defaultdict, deque
+
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 os.chdir(CURRENT_DIRECTORY)
@@ -23,7 +16,39 @@ def read_input_text():
 
 
 def part_a():
-    pass
+    drawing, operations = read_input_text().split("\n\n")
+    state = defaultdict(deque)  # {1 : deque["J", "A", "P" ...}
+
+    for row in drawing.split("\n")[::-1]:
+        for loc, symb in enumerate(row):
+            if symb.isupper():
+                state[(loc//4) + 1].append(symb)
+
+    for op in operations.split("\n"):
+        _, n, _, fromi, _, toi = op.split()
+        for _ in range(int(n)):
+            state[int(toi)].append(state[int(fromi)].pop())
+
+    print("".join((state[i].pop() for i in range(1,10))))
+
+
+
 
 def part_b():
-    pass
+    drawing, operations = read_input_text().split("\n\n")
+    state = defaultdict(deque)
+
+    for row in drawing.split("\n")[::-1]:
+        for loc, symb in enumerate(row):
+            if symb.isupper():
+                state[(loc // 4) + 1].append(symb)
+
+    for op in operations.split("\n"):
+        _, n, _, fromi, _, toi = op.split()
+        removed = [state[int(fromi)].pop() for _ in range(int(n))]
+        state[int(toi)].extend(removed[::-1])
+
+    print("".join((state[i].pop() for i in range(1, 10))))
+
+part_a()
+part_b()
