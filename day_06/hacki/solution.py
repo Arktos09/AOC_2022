a94e59435819cc28ab34c92058e1721a81b3c506
Sebastian Hackler <shackler@informationsfabrik.de>
2022-12-06 18:08:32 +0100

    reuced lines

--- day_06/hacki/solution.py
+++ day_06/hacki/solution.py
@@ -6,12 +6,8 @@ def get_fileinput(filename: str) -> list:
         content = f.readlines()
     return [x.strip('\n') for x in content]
 
-
-
-
 def shifter(code_in, start_shift: int, shift: int) -> set:
     return code_in[start_shift:shift]
-
 
 def get_first_signal(code_in:str, shift_chunk:int)-> int:
 
@@ -20,9 +16,9 @@ def get_first_signal(code_in:str, shift_chunk:int)-> int:
     shift_list = []
     while shifted < len(code_in)-shift_chunk:
         shift = shifter(code_in, shifted, shift_chunk+shifted)
+        signal = f'{signal}{code_in[4+shifted]}'
         shifted += 1
         shift_list.append(shift)
-    
     shift_list = [signal for signal in shift_list]
     
     return shift_chunk+[shift_list.index(i) for i in shift_list if len(set(i)) == shift_chunk][0]
@@ -31,11 +27,5 @@ def get_first_signal(code_in:str, shift_chunk:int)-> int:
 filename = 'debug.txt'
 filename = 'day_6_1.txt'
 puzzle_list = get_fileinput(filename)
-
-
-
-shift_chunk = 4
-a = [get_first_signal(code_in, shift_chunk) for code_in in puzzle_list]
-shift_chunk = 14
-b = [get_first_signal(code_in, shift_chunk) for code_in in puzzle_list]
+a,b = [[get_first_signal(code_in, shift_chunk) for code_in in puzzle_list] for shift_chunk in [4, 14]]
 print(a,b)
