a361063b28d27748af814e9d47c78e15a60d7d73
Michael Willig <michael.willig@gs1.nl>
2022-12-13 08:45:14 +0100

    day 13

--- day_13/michael/solution.py
+++ day_13/michael/solution.py
@@ -1,14 +1,5 @@
 import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
-from copy import deepcopy
-from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
-from functools import reduce
+import json
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 os.chdir(CURRENT_DIRECTORY)
@@ -21,9 +12,63 @@ def read_input_text():
     with open('input.txt', 'r') as fh:
         return fh.read().strip()
 
+RIGHT = 1
+WRONG = -1
+
+
+def checkpair(a, b):
+    if type(a) == type(b) == int:
+        return (a < b) - (b < a)
+
+    if type(a) == int:
+        a = [a]
+    if type(b) == int:
+        b = [b]
+
+    for newa, newb in zip(a, b):
+        result = checkpair(newa, newb)
+        if result:
+            return result
+
+    return (len(a) < len(b)) - (len(b) < len(a))
 
 def part_a():
-    pass
+
+    pairs = read_input_text().split("\n\n")
+    pairs = [[json.loads(y) for y in x.split("\n")] for x in pairs]
+
+    score = 0
+    for i, pair in enumerate(pairs):
+        if checkpair(*pair) == RIGHT:
+            score+= i + 1
+
+    print(score)
 
 def part_b():
-    pass
+    items = [json.loads(y) for y in read_input_text().split("\n") if y]
+    items += [[[2]], [[6]]]
+
+    def mergesort(items):
+        n = len(items)
+        itemsa, itemsb = items[:n//2], items[n//2:]
+        if n > 2:
+            itemsa, itemsb = mergesort(itemsa), mergesort(itemsb)
+
+        result = [] #sorted small -> large
+        while itemsa and itemsb:
+            head_comp = checkpair(itemsa[0], itemsb[0])
+            if head_comp == RIGHT:
+                result.append(itemsa[0])
+                itemsa = itemsa[1:]
+            else:
+                result.append(itemsb[0])
+                itemsb = itemsb[1:]
+
+        return result + itemsa + itemsb
+
+    in_order = mergesort(items)
+    jsonstrings = [json.dumps(x) for x in in_order]
+    print((jsonstrings.index("[[2]]")  + 1) * (jsonstrings.index("[[6]]") + 1))
+
+part_a()
+part_b()
