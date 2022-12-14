d99c02f3e8f1688c8ab3032ff56801c0fba3b457
Michael Willig <michael.willig@gs1.nl>
2022-12-14 08:38:45 +0100

    day 14

--- day_14/michael/solution.py
+++ day_14/michael/solution.py
@@ -1,14 +1,4 @@
 import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
-from copy import deepcopy
-from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
-from functools import reduce
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 os.chdir(CURRENT_DIRECTORY)
@@ -22,8 +12,61 @@ def read_input_text():
         return fh.read().strip()
 
 
+def drawmap():
+    map = {}
+
+    lines = read_input_lines()
+    for line in lines:
+        coords = []
+        for coord in line.split(" -> "):
+            a, b = coord.split(",")
+            coords.append(int(a) + 1j * int(b))
+
+        for pair in zip(coords[:-1], coords[1:]):
+            reals, imags = sorted([x.real for x in pair]), sorted([x.imag for x in pair])
+            range_real = range(int(reals[0]), int(reals[1] + 1))
+            range_imag = range(int(imags[0]), int(imags[1] + 1))
+            for real in range_real:
+                for imag in range_imag:
+                    map[real + imag * 1j] = "#"
+
+    return map
+
 def part_a():
-    pass
+    map = drawmap()
+    max_y = max((coord.imag for coord in map)) # larger y than this will never stop
+
+    ### walk
+    path = [500]
+    while path[-1].imag < max_y:
+        for dir in [1j, 1j -1, 1j+1]:
+            if (path[-1] + dir) not in map:
+                path.append(path[-1] + dir) # we fall further
+                break
+        else: # we come to rest
+            map[path.pop()] = "o"
+
+    print(len([v for v in map.values() if v == "o"]))
 
 def part_b():
-    pass
+
+    map = drawmap()
+    max_y = max((coord.imag for coord in map))  # larger y than this will never stop
+
+    ### walk
+    path = [500]
+    while path:
+        if path[-1].imag == (max_y + 1): # we come to rest:
+            map[path.pop()] = "o"
+
+        for dir in [1j, 1j - 1, 1j + 1]:
+            if (path[-1] + dir) not in map:
+                path.append(path[-1] + dir)  # we fall further
+                break
+        else:  # we come to rest
+            map[path.pop()] = "o"
+
+    print(len([v for v in map.values() if v == "o"]))
+
+
+part_b()
