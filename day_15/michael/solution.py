bbf1ad1dcde84c865e8021d19bdf3f9ed5ad0b14
Michael Willig <michael.willig@gs1.nl>
2022-12-16 10:40:28 +0100

    Not happy with my part 2, which uses greedy heuristics which i'm sure do not apply to all inputs

--- day_15/michael/solution.py
+++ day_15/michael/solution.py
@@ -1,16 +1,25 @@
 import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
-from copy import deepcopy
-from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
-from functools import reduce
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
+os.chdir(CURRENT_DIRECTORY)
+
+def read_input_lines_test():
+    return """Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3""".split("\n")
+
+
 os.chdir(CURRENT_DIRECTORY)
 
 def read_input_lines():
@@ -23,7 +32,74 @@ def read_input_text():
 
 
 def part_a():
-    pass
 
+    special_row = 2000000
+    beacons_special_row = set()
+    lines = read_input_lines()
+
+    # get ranges of points on relevant line
+    y2000000ranges = []
+    for line in [line for line in lines if line]:
+        sx, sy, bx,by = [int("".join([let for let in x.split("=")[-1] if let.isdigit() or let == '-'])) for x in line.split() if "=" in x]
+        dist = abs(sx-bx) + abs(sy-by)
+        y2000000_width_one_side = dist - abs(sy - special_row)
+        if y2000000_width_one_side >= 0: # 0 means we have a single point
+            y2000000ranges.append((sx-y2000000_width_one_side, sx + y2000000_width_one_side))
+        if by == special_row:
+            beacons_special_row.add((bx,by))
+
+    # merge ranges where possible
+    y2000000ranges = sorted(y2000000ranges)
+    merged_ranges = []
+
+    minx, maxx = y2000000ranges[0]
+    for x,x2 in y2000000ranges[1:]:
+        if x <= maxx: # start falls in prev range
+            if x2 > maxx:
+                maxx = x2
+            #else we already cover the entire range
+        else:
+            merged_ranges.append((minx, maxx))
+            minx, maxx = x, x2
+    merged_ranges.append((minx, maxx))
+    print(sum([x2  + 1- x for x, x2 in merged_ranges] ) - len(beacons_special_row))
+
+part_a()
 def part_b():
-    pass
+    """idea: we only need to check the rim coordinates around avery sensor area. because we know there is only one unchecked spot
+       possible optimization: early exclusion on cells outside range and """
+
+    def check_covered(x,y):
+        for sx, sy, dist in covered_areas:
+            if (abs(x-sx) + abs(y-sy)) <= dist:
+                return True
+
+    def get_coords_of_interest(sx,sy,dist):
+        dist+=1 # we are actually interested 1 stop beyond the sensor range
+        for xdir, ydir in [(1,1),(-1,1),(-1,-1),(1,-1)]:
+            for y_part in range(dist+1):
+                x_offset = (dist - y_part) * xdir
+                y_offset = y_part * ydir
+                yield (sx + x_offset, sy + y_offset)
+
+
+    lines = read_input_lines()
+
+    covered_areas = [] # tuples of sensors x,y,distance covered
+    for line in [line for line in lines if line]:
+        sx, sy, bx,by = [int("".join([let for let in x.split("=")[-1] if let.isdigit() or let == '-'])) for x in line.split() if "=" in x]
+        dist = abs(sx-bx) + abs(sy-by)
+        covered_areas.append((sx,sy,dist))
+
+    for area in covered_areas:
+        for poi in get_coords_of_interest(*area):
+            if not check_covered(*poi):
+                if all( 0 <= coord <= 4000000 for coord in poi):
+                    print(poi)
+                    print(poi[0] * 4000000 + poi[1])
+                    return
+
+
+
+
+part_b()
--- day_16/michael/solution.py
+++ day_16/michael/solution.py
@@ -1,14 +1,7 @@
 import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
-from copy import deepcopy
+
 from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
-from functools import reduce
+
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 os.chdir(CURRENT_DIRECTORY)
@@ -21,9 +14,91 @@ def read_input_text():
     with open('input.txt', 'r') as fh:
         return fh.read().strip()
 
+def readmap():
+    map = {}
+    for line in read_input_lines():
+        fromnode = line.split()[1]
+        flow = int(line.split("rate=")[1].split(";")[0])
+        outnodes = line.split("valves ")[-1].split(", ")
+        map[fromnode] = (flow, outnodes)
+
+    return map
 
 def part_a():
-    pass
+    """each state is defined by which valves are open the current pos and the current t
+       - we dont open flow rate 0s
+       - as a possible optimization we could cut states with the same pos and fewer valves open than another state and t<state t"""
+    map = readmap()
+    states = {("AA", tuple(), 0) : 0} # state -> score
+
+    current_states = {("AA", tuple(), 0)}
+    next_states = set()
+    for t in range(30):
+        for node, opened, _ in current_states:
+            flow, nextnodes = map[node]
+            if (not node in opened) and (flow > 0):
+                next_state = (node, tuple(sorted(opened + (node,))), t+1)
+                next_score = states[(node,opened, t)] + flow * (30 - (t + 1))
+                if next_score > (states.get(next_state) or -1):
+                    states[next_state] = next_score
+                    next_states.add(next_state)
+            for nextnode in nextnodes:
+                next_state = (nextnode, opened, t + 1)
+                next_score = states[(node, opened, t)]
+                if next_score > (states.get(next_state) or -1):
+                    states[next_state] = next_score
+                    next_states.add(next_state)
+        current_states = next_states
+        next_states = set()
+        #print([(s, states[s]) for s in current_states])
+        #if t == 5:
+        #    break
+    print(max(states.values()))
+
 
 def part_b():
-    pass
+    """each state is defined by which valves are open the current pos (2x) and the current t
+       - we dont open flow rate 0s
+       - as a possible optimization we could cut states with the same pos and fewer valves open than another state and t<state t"""
+
+
+    def get_max_score(state):
+        altstate = (state[0][::-1],*state[1:])
+        maxscore = -1
+        maxscore =  states[state] if state in states and states[state] > maxscore else maxscore
+        maxscore =  states[altstate] if altstate in states and states[altstate] > maxscore else maxscore
+        return maxscore
+
+    def filter_states(nextstates):
+        """we greedily only retain the 20k states with the best score"""
+        return set(sorted(list(nextstates), key= lambda x: states[x], reverse= True)[:20000])
+
+    map = readmap()
+    states = {(("AA", "AA"), tuple(), 0): 0}  # state -> score
+
+    current_states = {(("AA", "AA"), tuple(), 0)}
+    next_states = set()
+    for t in range(26 * 2):
+        posix = t % 2 #0 = me, 1 = elephant
+        for nodes, opened, _ in current_states:
+            node = nodes[posix]
+            flow, nextnodes = map[node]
+            if (not node in opened) and (flow > 0):
+                next_state = (nodes, tuple(sorted(opened + (node,))), t + 1)
+                next_score = states[(nodes, opened, t)] + flow * (26 - (t//2 + 1))
+                if next_score > get_max_score(next_state):
+                    states[next_state] = next_score
+                    next_states.add(next_state)
+            for nextnode in nextnodes:
+                nextnodes = tuple([node if i != posix else nextnode for i, node in enumerate(nodes) ])
+                next_state = (nextnodes, opened, t + 1)
+                next_score = states[(nodes, opened, t)]
+                if next_score > get_max_score(next_state):
+                    states[next_state] = next_score
+                    next_states.add(next_state)
+        current_states = filter_states(next_states)
+        next_states = set()
+        print(t, len(current_states))
+    print(max(states.values()))
+
+part_b()
