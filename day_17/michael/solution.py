297eefa13b85e413d8516b1473fb019dae1d87dd
Michael Willig <michael.willig@gs1.nl>
2022-12-18 10:15:09 +0100

    day 17 and 18. 17 was though.

--- day_17/michael/solution.py
+++ day_17/michael/solution.py
@@ -1,14 +1,5 @@
 import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
-from copy import deepcopy
-from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
-from functools import reduce
+from collections import Counter
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 os.chdir(CURRENT_DIRECTORY)
@@ -22,8 +13,116 @@ def read_input_text():
         return fh.read().strip()
 
 
-def part_a():
-    pass
+def simplifymap(map):
+    counts = Counter((x.real for x in map))
+    for k,v in counts.items():
+        if v == 7:
+            return {x for x in map if x.real >= v}
+    return map
 
+
+def part_a():
+    pat = read_input_text()
+    rock_types = [{1j * i for i in range(4) } , # as defined from lower left corner being 0, _
+                  {1j, 1, 1j+1, 1j+2, 2j+1}, # +
+                  {0, 1j, 2j, 2j+1, 2j+2}, # L
+                  set(range(4)), #|
+                  {0,1j,1,1j+1}] # #
+
+    min_x, max_x = -2, 4
+    highest_point = -1
+    floor = -1
+
+    map = {-1 + (x-2) * 1j  for x in range(7)}
+    windix = 0
+
+    for rocknr in range(2022):
+        rock = rock_types[rocknr%5]
+        rock = {point + highest_point + 4 for point in rock}
+        while True:
+            dwind = {">" : 1j, "<" : -1j}[pat[windix%len(pat)]]
+            blownrock = {point + dwind for point in rock}
+            if all((max_x >= point.imag >= min_x for point in blownrock)) and not (blownrock & map): # rock can move
+                rock = blownrock
+            windix += 1
+            droppedrock = {point -1 for point in rock}
+            if any((point.real <= floor for point in droppedrock )) or (droppedrock & map): #our rock would collide with floor or other rock
+                for point in rock:
+                    map.add(point)
+                highest_point = max((point.real for point in map))
+
+                break
+            else:
+                rock = droppedrock
+
+
+    print(max(point.real for point in map) + 1)
+
+part_a()
 def part_b():
-    pass
+
+
+    def get_state(map, maxh, windix,pat, rockix): # flat top, windindex = 0
+        top_view = []
+        for xcoord in range(7):
+            h = maxh
+            while True:
+                if h + (xcoord-2) * 1j in map:
+                    top_view.append(h)
+                    break
+                h-=1
+
+        top_view = tuple([h - maxh for h in top_view])
+        return (top_view, windix % len(pat),rockix % 5)
+
+    rock2score = {}
+    state2rocknr = {}
+
+    #pat = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"#
+    pat = read_input_text()
+    rock_types = [{1j * i for i in range(4)},  # as defined from lower left corner being 0, _
+                  {1j, 1, 1j + 1, 1j + 2, 2j + 1},  # +
+                  {0, 1j, 2j, 2j + 1, 2j + 2},  # L
+                  set(range(4)),  # |
+                  {0, 1j, 1, 1j + 1}]  # #
+
+    min_x, max_x = -2, 4
+    highest_point = -1
+
+    map = {-1 + (x-2) * 1j  for x in range(7)}
+    windix = 0
+    rocknr = 0
+
+    while True:
+        rock = rock_types[rocknr % 5]
+        rock = {point + highest_point + 4 for point in rock}
+        while True:
+            dwind = {">": 1j, "<": -1j}[pat[windix % len(pat)]]
+            blownrock = {point + dwind for point in rock}
+            if all((max_x >= point.imag >= min_x for point in blownrock)) and not (blownrock & map):  # rock can move
+                rock = blownrock
+            windix += 1
+            droppedrock = {point - 1 for point in rock}
+            if (droppedrock & map):  # our rock would collide with floor or other rock
+                for point in rock:
+                    map.add(point)
+                highest_point = max((point.real for point in map))
+                rocknr +=1
+                rock2score[rocknr] = highest_point + 1
+                state = get_state(map, highest_point, windix,pat, rocknr)
+                if state in state2rocknr:
+                    print("cycle found!")
+                    rocks_left = 1000000000000 - rocknr
+                    cycle_length = rocknr - state2rocknr[state]
+                    cycles, out_of_cycles = rocks_left//cycle_length, rocks_left % cycle_length
+                    cycle_value = (highest_point + 1) - rock2score[state2rocknr[state]]
+                    out_of_cycle_value = rock2score[state2rocknr[state]+out_of_cycles ] - rock2score[state2rocknr[state]]
+                    print(cycle_value * cycles + (highest_point + 1) + out_of_cycle_value)
+                    return
+                state2rocknr[state] = rocknr
+
+                break
+            else:
+                rock = droppedrock
+
+part_b()
--- day_18/michael/solution.py
+++ day_18/michael/solution.py
@@ -1,14 +1,5 @@
 import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
-from copy import deepcopy
-from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
-from functools import reduce
+from collections import defaultdict
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 os.chdir(CURRENT_DIRECTORY)
@@ -22,8 +13,43 @@ def read_input_text():
         return fh.read().strip()
 
 
+def get_adj(loc):
+        for dim in range(3):
+            for dif in [1,-1]:
+                yield tuple(loc[:dim] + tuple([loc[dim] + dif]) + loc[dim + 1:])
+
 def part_a():
-    pass
+    locs = {tuple([int(coord) for coord in coords.split(",")]) for coords in read_input_lines()}
+    surface = 0
+    for loc in locs:
+        for adj_loc in get_adj(loc):
+            if not adj_loc in locs:
+                surface += 1
+    print(surface)
+
 
 def part_b():
-    pass
+    locs = {tuple([int(coord) for coord in coords.split(",")]) for coords in read_input_lines()}
+    # coords are between 0 and 19 for each dim
+
+    surface = 0
+    visited = {(-1,-1,-1)}
+    current_air = {(-1,-1,-1)}
+    next_air = set()
+
+    while current_air:
+        for air in current_air:
+            for adj in get_adj(air):
+                if adj in locs:
+                    surface += 1
+
+                elif (max(adj) < 21) and (min(adj) > -2) and (adj not in visited):
+                    visited.add(adj)
+                    next_air.add(adj)
+        current_air = next_air
+        next_air = set()
+
+    print(surface)
+
+part_a()
+part_b()
